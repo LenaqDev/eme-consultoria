085abb633fafff3eadf1e0daba3572958a8cb365
LuisRodriguez <luisrodriguezacosta@gmail.com>
2026-01-29 22:31:29 -0500

    feat: Update favicon and project images
    - Add new favicon images and manifest for better branding.
    - Remove outdated SVG icons and replace project images with updated ones.
    - Update project data in `projects.ts` to reflect new images and correct IDs.

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -16,10 +16,10 @@ export const projects: Project[] = [
         status: "Finalizado",
         progress: "100%",
         category: "Medio Ambiente",
-        image: "/images/project-images/cocinas.jpg"
+        image: "/images/project-images/cocina-fresno.jpeg"
     },
     {
-        id: 4,
+        id: 2,
         location: "Cauca",
         title: "SISTEMA INTEGRADO TECNICO EDUCATIVO COMO HERRAMIENTA PARA EL USO Y APROPIACION DE LOS RECURSOS DIDÁCTICOS TIC EN INSTITUCIONES EDUCATIVAS DEL DEPARTAMENTO DEL CAUCA",
         status: "Finalizado",
@@ -29,13 +29,22 @@ export const projects: Project[] = [
     },
 
     {
-        id: 7,
+        id: 3,
         location: "Acevedo, Huila",
         title: "CONSTRUCCIÓN CUBIERTA POLIDEPORTIVO INSTITUCIÓN EDUCATIVA LA VICTORIA SEDE LA UNIÓN",
         status: "Finalizado",
         progress: "100%",
         category: "Infraestructura",
         image: "/images/project-images/cubierta2.jpeg"
+    },
+    {
+        id: 4,
+        location: "Campamento, Antioquia",
+        title: "DOTACIÓN DE ESTUFAS ECOEFICIENTES PARA FAMILIAS EN ZONAS RURALES DEL MUNICIPIO DE CAMPAMENTO ANTIOQUIA",
+        status: "Finalizado",
+        progress: "100%",
+        category: "Medio Ambiente",
+        image: "/images/project-images/estufa-campamento-antioquiav2.jpeg"
     },
     {
         id: 5,
@@ -47,27 +56,45 @@ export const projects: Project[] = [
         image: "/images/project-images/restauracion.jpg"
     },
     {
-        id: 3,
+        id: 5,
+        location: "Tópaga, Boyacá",
+        title: "IMPLEMENTACIÓN DE ESTUFAS ECOEFICIENTES PARA FAMILIAS DE LA ZONA RURAL COMO MEDIDA DE CONSERVACIÓN AMBIENTAL EN EL MUNICIPIO DE TÓPAGA DEPARTAMENTO DE BOYACÁ",
+        status: "En Ejecución",
+        progress: "En Ejecución",
+        category: "Medio Ambiente",
+        image: "/images/project-images/estufa-topaga-boyaca.jpeg"
+    },
+    {
+        id: 6,
         location: "Algeciras, Huila",
         title: "IMPLEMENTACIÓN DE ESTUFAS ECOEFICIENTES COMO MEDIDA DE CONSERVACIÓN AMBIENTAL",
         status: "Finalizado",
         progress: "100%",
         category: "Medio Ambiente",
-        image: "/images/project-images/cocinas.jpg"
+        image: "/images/project-images/cocina-ecologica.jpeg"
     },
     {
-        id: 2,
+        id: 7,
+        location: "Palestina, Huila",
+        title: "IMPLEMENTACIÓN ESTUFAS ECOEFICIENTES PARA FAMILIAS DE LA ZONA RURAL COMO MEDIDA DE CONSERVACIÓN AMBIENTAL EN EL MUNICIPIO DE PALESTINA DEPARTAMENTO DEL HUILA",
+        status: "Finalizado",
+        progress: "100%",
+        category: "Medio Ambiente",
+        image: "/images/project-images/cocina-palestina.jpeg"
+    },
+    {
+        id: 8,
         location: "Acevedo, Huila",
         title: "IMPLEMENTACIÓN DE ESTUFAS ECOEFICIENTES COMO MEDIDA DE CONSERVACIÓN AMBIENTAL",
         status: "Finalizado",
         progress: "100%",
         category: "Medio Ambiente",
-        image: "/images/project-images/cocinas.jpg"
+        image: "/images/project-images/estufa-ecologica2.jpeg"
     },
 
 
     {
-        id: 6,
+        id: 9,
         location: "Chinchiná, Caldas",
         title: "IMPLEMENTACIÓN DE ACCIONES DE RESTAURACIÓN ACTIVA Y CONSERVACIÓN EN AREAS DECLARADAS DE IMPORTANCIA ESTRATEGICA PARA LA CONSERVACIÓN DEL RECURSO HIDRICO",
         status: "Finalizado",
@@ -77,75 +104,50 @@ export const projects: Project[] = [
     },
 
     {
-        id: 8,
+        id: 10,
         location: "Puerto Guzmán, Putumayo",
         title: "IMPLEMENTACIÓN DE ESTUFAS ECOEFICIENTES PARA FAMILIAS DE LA ZONA RURAL COMO MEDIDA DE CONSERVACIÓN AMBIENTAL EN EL MUNICIPIO DE PUERTO GUZMÁN, PUTUMAYO",
         status: "Finalizado",
         progress: "100%",
         category: "Medio Ambiente",
-        image: "/images/project-images/cocinas.jpg"
+        image: "/images/project-images/estufa-ecologica2.jpeg"
     },
     {
-        id: 9,
+        id: 11,
         location: "Acevedo, Huila",
         title: "DOTACION DE ESTUFAS ECOEFICIENTES PARA FAMILIAS EN ZONAS RURALES DEL MUNICIPIO DE ACEVEDO, DEPARTAMENTO DEL HUILA",
         status: "Finalizado",
         progress: "100%",
         category: "Medio Ambiente",
-        image: "/images/project-images/cocinas.jpg"
+        image: "/images/project-images/cocina-ecologica.jpeg"
     },
+    
     {
-        id: 10,
-        location: "Campamento, Antioquia",
-        title: "DOTACIÓN DE ESTUFAS ECOEFICIENTES PARA FAMILIAS EN ZONAS RURALES DEL MUNICIPIO DE CAMPAMENTO ANTIOQUIA",
-        status: "Finalizado",
-        progress: "100%",
-        category: "Medio Ambiente",
-        image: "/images/project-images/cocinas.jpg"
-    },
-    {
-        id: 11,
+        id: 12,
         location: "Chinchiná",
         title: "IMPLEMENTACIÓN DE POZOS SÉPTICOS PARA LA CONSERVACIÓN PROTECCIÓN Y PRESERVACIÓN DEL SUELO Y LOS RECURSOS HÍDRICOS EN LA ZONA RURAL DEL MUNICIPIO DE CHINCHINÁ",
         status: "Finalizado",
         progress: "100%",
         category: "Agua y Saneamiento",
-        image: "/images/project-images/cocinas.jpg"
+        image: "/images/project-images/pozo-septico.jpg"
     },
-    {
-        id: 12,
-        location: "Palestina, Huila",
-        title: "IMPLEMENTACIÓN ESTUFAS ECOEFICIENTES PARA FAMILIAS DE LA ZONA RURAL COMO MEDIDA DE CONSERVACIÓN AMBIENTAL EN EL MUNICIPIO DE PALESTINA DEPARTAMENTO DEL HUILA",
-        status: "Finalizado",
-        progress: "100%",
-        category: "Medio Ambiente",
-        image: "/images/project-images/cocinas.jpg"
-    },
+    
     {
         id: 13,
-        location: "Tópaga, Boyacá",
-        title: "IMPLEMENTACIÓN DE ESTUFAS ECOEFICIENTES PARA FAMILIAS DE LA ZONA RURAL COMO MEDIDA DE CONSERVACIÓN AMBIENTAL EN EL MUNICIPIO DE TÓPAGA DEPARTAMENTO DE BOYACÁ",
-        status: "En Ejecución",
-        progress: "En Ejecución",
-        category: "Medio Ambiente",
-        image: "/images/project-images/cocinas.jpg"
-    },
-    {
-        id: 14,
         location: "Isnos, Huila",
         title: "IMPLEMENTACIÓN DE ESTUFAS ECOEFICIENTES PARA FAMILIAS DE LA ZONA RURAL COMO MEDIDA DE CONSERVACIÓN AMBIENTAL EN EL MUNICIPIO DE ISNOS, DEPARTAMENTO DEL HUILA",
         status: "En Ejecución",
         progress: "En Ejecución",
         category: "Medio Ambiente",
-        image: "/images/project-images/cocinas.jpg"
+        image: "/images/project-images/estufa-ecologica2.jpeg"
     },
     {
-        id: 15,
+        id: 14,
         location: "San Pedro de Cartago, Nariño",
         title: "IMPLEMENTACIÓN DE ESTUFAS ECOEFICIENTES PARA FAMILIAS DE LA ZONA RURAL COMO MEDIDA DE CONSERVACIÓN AMBIENTAL EN EL MUNICIPIO SAN PEDRO DE CARTAGO, DEPARTAMENTO DE NARIÑO",
         status: "En Ejecución",
         progress: "En Ejecución",
         category: "Medio Ambiente",
-        image: "/images/project-images/cocinas.jpg"
+        image: "/images/project-images/cocina-ecologica.jpeg"
     }
 ];
